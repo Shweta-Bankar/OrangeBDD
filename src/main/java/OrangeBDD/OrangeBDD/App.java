b6a182287349ec44fd62856921c10b389c933202
Shweta-Bankar <156903478+Shweta-Bankar@users.noreply.github.com>
2024-01-19 15:26:05 +0530

    Framework is created

---
+++ src/main/java/OrangeBDD/OrangeBDD/App.java
@@ -0,0 +1,13 @@
+package OrangeBDD.OrangeBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/RunnerTest/TestRunner.java
@@ -0,0 +1,17 @@
+package RunnerTest;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {"AllFeatureFiles"},
+		glue= {"Steps"},
+		plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		)
+
+public class TestRunner {
+
+}
